da265bd3719e058fe57247f8f60a24785b47bf57
User <User@DESKTOP-8KK9T6M>
2025-05-14 05:34:23 -0700

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿
+var a = ReturnNumbers(10);
+
+//foreach (var item in ReturnNumbers(10))
+//{
+//    Console.WriteLine(item);
+//}
+IEnumerable<int> ReturnNumbers(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+       yield return i+1;
+    }
+
+}
